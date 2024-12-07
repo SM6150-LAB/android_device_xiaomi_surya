57959313d54d99e40201465d33089497d5ace66a
Nick Reuter <nreuter85@gmail.com>
2024-12-07 15:59:17 -0600

    surya: Bump libwfdservice to android.media.audio.common.types-V4
    Change-Id: I81060d8c73430a9598c676e18dc04e31a2f31c0c

--- extract-files.py
+++ extract-files.py
@@ -54,6 +54,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'(start|stop) wfdservice\b', r'\1 wfdservice64'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'vendor/etc/init/init.batterysecret.rc': blob_fixup()
         .regex_replace(' +seclabel u:r:batterysecret:s0\n', ''),
     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
