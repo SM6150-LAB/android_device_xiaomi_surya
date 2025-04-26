crdroidandroid/device_xiaomi_surya

surya: Bring back QCOM WFD
This reverts commit 4a838e1a9bde7fe16c802e26f1c4c9d27e8b200f and d5d80a5347fc9a38cf3a5c08ed38a65e565c3b01.

Change-Id: Ib6b2ffc341fc22511ebacdd6b716257090209b3b

BoardConfig.mk
@@ -66,6 +66,7 @@ DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_mani
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += surya
 ODM_MANIFEST_SURYA_FILES := $(DEVICE_PATH)/configs/hidl/manifest-nfc.xml
+ODM_MANIFEST_SURYA_FILES += $(DEVICE_PATH)/configs/hidl/manifest-qva.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_surya

SM6150-LAB/android_device_xiaomi_surya

BoardConfig.mk
@@ -76,6 +76,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+ODM_MANIFEST_FILES += $(DEVICE_PATH)/configs/hidl/manifest-qva.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_surya