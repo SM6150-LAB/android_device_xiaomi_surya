87735f624f9ff2efe4876706cdd278976888c550
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2025-04-27 01:53:40 +0530

    surya: extract-files: Modify regex for dropping selinux context

--- extract-files.py
+++ extract-files.py
@@ -54,9 +54,9 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'vendor/etc/init/init.batterysecret.rc': blob_fixup()
-        .regex_replace(' +seclabel u:r:batterysecret:s0\n', ''),
+        .regex_replace(r'\s+seclabel u:r:batterysecret:s0', ''),
     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
-        .regex_replace(' +seclabel u:r:mi_thermald:s0\n', ''),
+        .regex_replace(r'\s+seclabel u:r:mi_thermald:s0', ''),
     'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
     ('vendor/lib64/libalAILDC.so', 'vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
