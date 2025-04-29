95c02c0a6225c46ee597cac1bddb0904c8dc9d56
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2025-04-29 12:35:32 +0530

    surya: Drop BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES for GApps builds

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,10 +16,6 @@
 
 DEVICE_PATH := device/xiaomi/surya
 
-ifeq ($(WITH_GMS),true)
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-endif
-
 # Inherit from proprietary files
 include vendor/xiaomi/surya/BoardConfigVendor.mk
 
