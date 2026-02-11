48e2beb98f29314ac444bf0b7499d099e70b3b66
mui09321 <mui09321@gmail.com>
2026-02-11 15:23:16 +0800

    surya: switch to latest clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,6 +114,10 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_ADDITIONAL_FLAGS += \
     AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+TARGET_KERNEL_CLANG_COMPILE := true
+KERNEL_CC := CC=clang
+TARGET_KERNEL_CLANG_VERSION := $(shell ls -d prebuilts/clang/host/linux-x86/clang-r* | sort -V | tail -n 1 | xargs basename)
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/$(TARGET_KERNEL_CLANG_VERSION)
 
 TARGET_KERNEL_APPEND_DTB := false
 TARGET_KERNEL_CLANG_COMPILE := true
