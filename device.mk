003c35732dda7e774b2a490a0138febe92667a37
mui09321 <mui09321@gmail.com>
2026-02-11 17:28:40 +0800

    Revert "surya: add signing keys"
    This reverts commit e1e070a9c887a475cdbcd509476fd4b7976eced4.

--- device.mk
+++ device.mk
@@ -365,9 +365,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
-# Signing
-$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
